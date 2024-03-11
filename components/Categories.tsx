5233356ee2fc18ee3515f706076806e20b4eb669
Oluwaseyi9325 <adedokunseyi96@gmail.com>
2024-03-11 17:48:09 +0100

    create categories components

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -1,10 +1,26 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity ,Image} from 'react-native'
 import React from 'react'
+import { categories } from '../constants'
 
 export default function Categories() {
   return (
-    <View>
-      <Text>Categories</Text>
+    <View className='mt-4'>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false} className='overflow-visible' contentContainerStyle={{
+        paddingHorizontal:20
+      }}>
+            {
+                categories.map((category,index)=>{
+                    return(
+                        <View key={index} className='flex justify-center items-center mr-6'>
+                            <TouchableOpacity className='p-1 rounded-full shadow bg-gray-200'>
+                                <Image style={{width:45,height:45}} source={category.image} />
+                            <Text>{category.name}</Text>
+                            </TouchableOpacity>
+                        </View>
+                    )
+                })
+            }
+      </ScrollView>
     </View>
   )
 }
---
+++ constants/index.ts
@@ -0,0 +1,34 @@
+export const categories=[
+    {
+        id:1,
+        name:"Pizza",
+        image:require("../assets/images/pizza.png")
+    },
+    {
+        id:2,
+        name:"Burger",
+        image:require("../assets/images/pizza.png")
+    },
+    {
+        id:3,
+        name:"Noodles",
+        image:require("../assets/images/pizza.png")
+    },
+    {
+        id:4,
+        name:"Chinese",
+        image:require("../assets/images/pizza.png")
+    },
+    {
+        id:5,
+        name:"Chinese",
+        image:require("../assets/images/pizza.png")
+    }
+]
+
+
+export const featured={
+    id:1,
+    title:"Hot and Spicy",
+    desciption:"Soft and tender friend chicken"
+}
